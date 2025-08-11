983c5254ed5c6a95a192f0a229d9f4fc0b2404c8
Prabinthapadev21 <codinghub57@gmail.com>
2025-08-11 14:26:36 +0545

    updated

---
+++ index.py
@@ -0,0 +1,21 @@
+# name = "Prabin"
+# print(f"Name is {name}")
+from builtins import function
+from math import factorial
+
+# a = 10
+# b = 20
+# c =30
+
+# if a>b and a>c:
+#     print(f"{a} is greater")
+# elif b>a and b>c:
+#     print(f"{b} is greater")
+# else:
+#     print(f"{c} is greater")
+
+# function in python
+function factorial()
+{
+
+}

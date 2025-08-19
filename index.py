a384f49fd655d659cb5974a25699203fdd5994c4
Prabinthapadev21 <codinghub57@gmail.com>
2025-08-19 18:34:48 +0545

    updated

--- index.py
+++ index.py
@@ -20,3 +20,57 @@
 # name = "i am prabin thapa"
 # for i in name:
 #     print(i)
+
+# length = int(input("Enter the length:"))
+# breadth = int(input("Enter the breadth:"))
+# perimeter = 2*(length*breadth)
+# print(f"The perimeter is {perimeter}")
+
+
+# print(2*3)
+# print(2**3)
+
+# string = "hello my name is prabin thapa"
+# count = 1
+# for i in string:
+#     if i == " ":
+#         count+=1
+
+# print(count)
+# s1 = 4
+# s2 = 5
+# s3 = 5
+# semiperimeter = (s1+s2+s3)/2
+# area = (semiperimeter*(semiperimeter-s1)(semiperimeter-s2)(semiperimeter-s3))**(1/2)
+# print(f" Area is {area}")
+# n = 5
+# d = 0
+# try:
+#  div = n/d
+#  print(div)
+# except ArithmeticError:
+#  print("Number cannot ne divided by zero")
+
+# finally:
+#  print(9/5) 
+
+
+# list
+
+# list = [1,'a',"prabin",4+5]
+# print(list)
+# print(type(list))
+# list.append("hello")
+# print(list)
+# for l in list:
+#     print(l)
+
+# list =[]
+# for i in range(1,10):
+#     if i % 2 == 0:
+#         list.append(i)
+# print(list)
+
+# l1 = list+ ["lict","hello","tu"]
+# print(l1)
+
